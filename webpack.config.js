8359f8e246dd3949fc8db24cdcbf09b7c5f8d884
Sahian Montalvo <msahianmontalvoo@gmail.com>
2024-07-29 17:19:55 -0600

    Estructura y config del proyecto

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require( 'path' );
+
+module.exports = {
+    entry: './src/index.js', // Punto de entrada de la aplicación
+    output: {
+        filename: ' bundle.js ', // Nombre del archivo de salida
+        path: path.resolve(__dirname, 'dist'), // Carpeta de salida
+    },
+    module: {
+        rules:[
+            {
+                test: /\.css$/, //Regex para identificar archivos CSS
+                use: ['style-loader', 'css-loader'], //Loaders para procesar archivos CSS
+            },
+            {
+                test: /\.js$/, //Regex para identificar archivos JS
+                exclude: /node_modules/, //Excluir la carpeta node_modules
+                use: {
+                    loader: 'babel-loader', //Loader para pasar JS moderno a JS más antiguo
+                    options: {
+                        presets: ['@babel/preset-env']
+                    },
+                },
+            },
+        ],
+    },
+    devtool: 'source-map', //Generar source maps para facilitar la depuración
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'), //Carpeta principal del servidor
+        compress: true, //Habilitar la compresión gzip
+        port: 9000, // Puerto del servidor de desarrollo
+    },
+};
